33015b51a18c98b180570bc62e2e7908426c1a77
mak-kelly <martin.a.kelly.28@dartmouth.edu>
2025-04-29 22:09:19 -0400

    added zustand

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.scss';
+import Counter from './components/counter';
+import Controls from './components/controls';
 
 function App() {
-  return <div className="test">All the REACT are belong to us!</div>;
+//  return <div className="test">All the REACT are belong to us!</div>;
+  return (
+    <div>
+      <Counter />
+      <Controls />
+    </div>
+  );
 }
 
 const root = createRoot(document.getElementById('main'));
