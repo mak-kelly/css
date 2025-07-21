fa0bb7675ef54fffbee42b47b74b113430656b22
mak-kelly <martin.a.kelly.28@dartmouth.edu>
2025-07-21 11:52:34 -0400

    basic nav bar finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Nav from './Nav';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
 
 export default About;
