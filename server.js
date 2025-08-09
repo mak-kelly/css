ec0ded49784ab52c9eb51aa23ff1497f1c21dcb3
mak-kelly <martin.a.kelly.28@dartmouth.edu>
2025-08-09 18:13:13 -0400

    fixed success and cancel rerouting to localhost

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.post('/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:5173/success',
-      cancel_url: 'http://localhost:5173/cancel',
+      success_url: `http://${CLIENT_URL}/success`,
+      cancel_url: `http://${CLIENT_URL}/cancel`,
     });
     res.json({ url: session.url });
   } catch (e) {
