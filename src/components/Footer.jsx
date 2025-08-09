ba2846c6996f2b7eff5e0f4a81a059850eda0aa5
mak-kelly <martin.a.kelly.28@dartmouth.edu>
2025-08-09 15:40:51 -0400

    edited footer to be more unique

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,29 +11,31 @@ function Footer() {
 
   return (
     <div>
-      <section id="footer-section">
-        <div id="contact-us">
-          <h2 id="contact-us-header">Contact Us!</h2>
-          <div id="contact-us-text">
-            <a href="tel:1-800-382-4360">TOLL-FREE: 1-800-382-4360</a>
-            <a href="tel:617-367-9046">Local: 617-367-9046</a>
-            <p>Email: <a href="mailto:info@cssc.com">info@cssc.com</a></p>
-            <p>Address:&nbsp;
-              <a href="https://maps.app.goo.gl/Mad6wuUkUzwd5c2U7">
-                54 Charles St, Boston, MA 02114
-              </a>
-            </p>
+      <div id="footer-containter">
+        <section id="footer-section">
+          <div id="contact-us">
+            <h2 id="contact-us-header">Contact Us!</h2>
+            <div id="contact-us-text">
+              <a href="tel:1-800-382-4360">TOLL-FREE: 1-800-382-4360</a>
+              <a href="tel:617-367-9046">Local: 617-367-9046</a>
+              <p>Email: <a href="mailto:info@cssc.com">info@cssc.com</a></p>
+              <p>Address:&nbsp;
+                <a href="https://maps.app.goo.gl/Mad6wuUkUzwd5c2U7">
+                  54 Charles St, Boston, MA 02114
+                </a>
+              </p>
+            </div>
           </div>
-        </div>
-        <div id="hours">
-          <h2 id="hours-header">Hours</h2>
-          <div id="hours-text">
-            <p>Monday - Friday: 8:00 AM - 6:00 PM</p>
-            <p>Saturday: 9:00 AM - 5:00 PM</p>
-            <p>Sunday: 11:00 AM - 4:00 PM</p>
+          <div id="hours">
+            <h2 id="hours-header">Hours</h2>
+            <div id="hours-text">
+              <p>Monday - Friday: 8:00 AM - 6:00 PM</p>
+              <p>Saturday: 9:00 AM - 5:00 PM</p>
+              <p>Sunday: 11:00 AM - 4:00 PM</p>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
       <section id="footer-bottom">
         <img id="footer-image" src="/footer.png" alt="Drawing of Charles Street Supply Storefront" />
         <img
